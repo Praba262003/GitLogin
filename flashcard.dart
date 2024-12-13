bfeacf3c61c85882e79fd8d82c9f7ba6dd0d9b62
Prabakaran S <140585995+Praba262003@users.noreply.github.com>
2024-12-13 09:32:26 +0530

    Update flashcard.dart

--- flashcard.dart
+++ flashcard.dart
@@ -1,6 +1,22 @@
 class Flashcard {
+  String id;
   String question;
   String answer;
 
-  Flashcard({required this.question, required this.answer});
+  Flashcard({required this.id, required this.question, required this.answer});
+
+  factory Flashcard.fromFirestore(Map<String, dynamic> data, String id) {
+    return Flashcard(
+      id: id,
+      question: data['question'],
+      answer: data['answer'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'question': question,
+      'answer': answer,
+    };
+  }
 }
